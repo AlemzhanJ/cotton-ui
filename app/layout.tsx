19b96e9e5968be53f9c25f08139d436a19eb229e
AlemzhanJ <alemjan377@gmail.com>
2025-12-19 20:25:21 +0500

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,9 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" style={{ backgroundColor: '#f0eeeb' }}>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-cotton-gray-100`}
       >
         {children}
       </body>
